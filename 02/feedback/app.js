c7004de8cca329b3c61586aaa47ac55268574f4b
Jiangzoe <1097288257@qq.com>
2020-01-13 20:09:04 +0800

    day02

--- 02/feedback/app.js
+++ 02/feedback/app.js
@@ -1,5 +1,34 @@
 var http = require('http')
 var fs = require('fs')
+var template = require('art-template')
+
+var comments = [
+    {
+        name:'一只酱1',
+        message:'我即是狂澜，且无法被力挽',
+        dateTime:'2020-01-13'
+    },
+    {
+        name:'一只酱2',
+        message:'我即是狂澜，且无法被力挽',
+        dateTime:'2020-01-13'
+    },
+    {
+        name:'一只酱3',
+        message:'我即是狂澜，且无法被力挽',
+        dateTime:'2020-01-13'
+    },
+    {
+        name:'一只酱4',
+        message:'我即是狂澜，且无法被力挽',
+        dateTime:'2020-01-13'
+    },
+    {
+        name:'一只酱5',
+        message:'我即是狂澜，且无法被力挽',
+        dateTime:'2020-01-13'
+    }
+]
 
 http
     .createServer(function(req, res) {
@@ -9,7 +38,10 @@ http
                 if (err) {
                     return res.end('404 not found')
                 }
-                res.end(data)
+                var htmlStr = template.render(data.toString(), {
+                    comments: comments
+                })
+                res.end(htmlStr)
             })
         } else if (url.indexOf('/public/') === 0) {
             fs.readFile('.' + url,function (err, data) {
